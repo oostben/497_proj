16ae000bddfb4b523b9a8c9c4d6fc1fc7f682d9d
oostben <oostben@umich.edu>
2020-10-22 16:51:18 -0400

    creation edit delete university/event

--- project/ticketswap/admin.py
+++ project/ticketswap/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, University, Event, Listing, Transaction
 
 admin.site.register(User, UserAdmin)
+admin.site.register(University)
+admin.site.register(Event)
+admin.site.register(Listing)
+admin.site.register(Transaction)
--- project/ticketswap/views.py
+++ project/ticketswap/views.py
@@ -2,14 +2,48 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import CustomUserCreationForm
+from .models import Event, University
+
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class EventCreate(CreateView):
+    model = Event
+    fields = ['name', 'time', 'location', 'description','universities']
+    success_url = '/ticketswap/'
+
+
+class EventUpdate(UpdateView):
+    model = Event
+    fields = ['name', 'time', 'location', 'description', 'universities']
+    success_url = '/ticketswap/'
+
+class EventDelete(DeleteView):
+    model = Event
+    success_url = reverse_lazy('/ticketswap/')
+
+#///////////////////////////////////////////
+
+class UniversityCreate(CreateView):
+    model = University
+    fields = ['name']
+    success_url = '/ticketswap/'
+
+
+class UniversityUpdate(UpdateView):
+    model = University
+    fields = ['name']
+    success_url = '/ticketswap/'
+
+
+class UniversityDelete(DeleteView):
+    model = University
+    success_url = reverse_lazy('/ticketswap/')
 
 def index(request):
     return HttpResponse("Hello! Welcome to the index of ticketswap")
