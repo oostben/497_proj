a7943348314daf4b191c9cdf2ee0fab3b4decafe
Paul Gossman <me@paulgossman.com>
2020-10-22 21:36:36 -0400

    [User creation] New validation rule to force email to match university domain name

--- project/ticketswap/forms.py
+++ project/ticketswap/forms.py
@@ -1,12 +1,28 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import User
+from .models import University, User
 
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('username', 'email', 'university')
+
+    def clean(self):
+        response = super().clean()
+
+        uni = self.cleaned_data["university"]
+        email = self.cleaned_data["email"]
+        email_domain_name = email.split("@")[-1]
+
+        breakpoint()
+
+        if uni.domain_name != email_domain_name:
+            raise forms.ValidationError(f"Email is not a valid {uni.name} email. Must have {uni.domain_name} domain name.")
+
+
+        return response
+
 
 class CustomUserChangeForm(UserChangeForm):
 
